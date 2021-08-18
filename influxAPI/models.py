383c69eed454322f02ab6781ad1933c77afa698e
Abilash <abilashenfield@gmail.com>
2021-08-18 19:10:03 +0530

    models added

--- influxAPI/models.py
+++ influxAPI/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Datas(models.Model):
+    data = models.TextField(null=True, blank=True)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.time
--- influxAPI/views.py
+++ influxAPI/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .connector import *
+from . models import Datas
 
 # Create your views here.
 
@@ -7,7 +8,17 @@ from .connector import *
 def getData(request):
     if request.method == 'POST':
         data = request.POST['data']
-        connectToInfluxDB(data)
+        Datas.objects.create(data=data)
+        # connectToInfluxDB(data)
+        return render(request, 'influxAPI/home.html')
+
+    else:
+        return render(request, 'influxAPI/home.html')
+
+
+def rawData(request, rdata):
+    if request.method == 'POST':
+        Datas.objects.create(data=rdata)
         return render(request, 'influxAPI/home.html')
 
     else:
