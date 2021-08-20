a0f4a221f36f91074bdb47d7d2c625fd5ba6dca9
Abilash <abilashenfield@gmail.com>
2021-08-20 16:49:01 +0530

    for AWS

--- influxAPI/urls.py
+++ influxAPI/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getData, name='getdata'),
-    path('rawdata/<str:rdata>', views.rawData, name='getdata'),
+    path('<str:rdata>', views.getData, name='getdata'),
+    path('rawdata/<str:rdata>', views.rawData, name='getrdata'),
 
 ]
--- influxAPI/views.py
+++ influxAPI/views.py
@@ -5,21 +5,12 @@ from . models import Datas
 # Create your views here.
 
 
-def getData(request):
-    if request.method == 'POST':
-        data = request.POST['data']
-        Datas.objects.create(data=data)
-        # connectToInfluxDB(data)
-        return render(request, 'influxAPI/home.html')
-
-    else:
-        return render(request, 'influxAPI/home.html')
+def getData(request, rdata):
+    Datas.objects.create(data=rdata)
+    return render(request, 'influxAPI/home.html')
 
 
 def rawData(request, rdata):
-    if request.method == 'POST':
-        Datas.objects.create(data=rdata)
-        return render(request, 'influxAPI/home.html')
+    Datas.objects.create(data=rdata)
+    return render(request, 'influxAPI/home.html')
 
-    else:
-        return render(request, 'influxAPI/home.html')
