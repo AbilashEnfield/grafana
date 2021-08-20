fbd52b4df4120c09428981264b55bbb17b64b29d
Abilash <abilashenfield@gmail.com>
2021-08-20 21:27:32 +0530

    slicer added

---
+++ influxAPI/slicer.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+
+def data_slicer(data):
+    rawSplit = data.split("&")
+    splited_data_list = []
+    for item in rawSplit:
+        splited_data = item.split("=")
+        splited_data_list.append(splited_data)
+
+    # print(splited_data_list)
+    Column_name = []
+    Column_value = []
+    for each in splited_data_list:
+        Column_name.append(each[0])
+        Column_value.append(each[1])
+
+    final_data = dict(zip(Column_name, Column_value))
+
+    print(final_data)
+
+    df = pd.DataFrame(data=final_data, index=[0])
+
+    df = df.T
+
+    # print(df)
+
+    df.to_excel('dict1.xlsx')
+
+    return final_data
--- influxAPI/views.py
+++ influxAPI/views.py
@@ -1,16 +1,77 @@
 from django.shortcuts import render
 from .connector import *
-from . models import Datas
+from .slicer import data_slicer
+from .models import Datas
+
 
 # Create your views here.
 
-
 def getData(request, rdata):
-    Datas.objects.create(data=rdata)
+    processed_data = data_slicer(rdata)
+
+    Datas.objects.create(
+        header=processed_data["vii-pxpy-uqaheader"],
+        customer_id=processed_data["id"],
+        imei_number=processed_data["imei"],
+        time=processed_data["time"],
+        di1=processed_data["di1"],
+        di2=processed_data["di2"],
+        di3=processed_data["di3"],
+        di4=processed_data["di4"],
+        do1=processed_data["do1"],
+        do2=processed_data["do2"],
+        do3=processed_data["do3"],
+        do4=processed_data["di4"],
+        ai1=processed_data["ai1"],
+        ai2=processed_data["ai2"],
+        ai3=processed_data["ai3"],
+        ai4=processed_data["ai4"],
+        modbus1=processed_data["modbus1"],
+        modbus2=processed_data["modbus2"],
+        modbus3=processed_data["modbus3"],
+        mcc=processed_data["mcc"],
+        mnc=processed_data["mnc"],
+        cell_id=processed_data["cellid"],
+        ver=processed_data["ver"],
+        bat=processed_data["bat"],
+        pwrsts=processed_data["pwrsts"],
+        gsmver=processed_data["gsmver"],
+        gsmsig=processed_data["gsmsig"],
+        end=processed_data["end"]
+    )
     return render(request, 'influxAPI/home.html')
 
 
 def rawData(request, rdata):
-    Datas.objects.create(data=rdata)
+    processed_data = data_slicer(rdata)
+    Datas.objects.create(
+        header=processed_data["vii-pxpy-uqaheader"],
+        customer_id=processed_data["id"],
+        imei_number=processed_data["imei"],
+        time=processed_data["time"],
+        di1=processed_data["di1"],
+        di2=processed_data["di2"],
+        di3=processed_data["di3"],
+        di4=processed_data["di4"],
+        do1=processed_data["do1"],
+        do2=processed_data["do2"],
+        do3=processed_data["do3"],
+        do4=processed_data["di4"],
+        ai1=processed_data["ai1"],
+        ai2=processed_data["ai2"],
+        ai3=processed_data["ai3"],
+        ai4=processed_data["ai4"],
+        modbus1=processed_data["modbus1"],
+        modbus2=processed_data["modbus2"],
+        modbus3=processed_data["modbus3"],
+        mcc=processed_data["mcc"],
+        mnc=processed_data["mnc"],
+        cell_id=processed_data["cellid"],
+        ver=processed_data["ver"],
+        bat=processed_data["bat"],
+        pwrsts=processed_data["pwrsts"],
+        gsmver=processed_data["gsmver"],
+        gsmsig=processed_data["gsmsig"],
+        end=processed_data["end"]
+    )
     return render(request, 'influxAPI/home.html')
-
